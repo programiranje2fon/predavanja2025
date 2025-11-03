76ab9a6cd667e44f59de3cb6d6c5ebf8b3dcf417
bojantomic <tomicb@fon.rs>
2025-11-03 10:01:36 +0100

    Dodato resenje za predavanje 1

---
+++ src/dvocas1_ponedeljak/zadatak1/Osoba.java
@@ -0,0 +1,29 @@
+package dvocas1_ponedeljak.zadatak1;
+/*
+    Predstavlja klasu Osoba koja ima ime, prezime
+    pol, godinu rodjenja i da li je u braku.
+ */
+class Osoba {
+    // int double char boolean String
+    String ime;
+    String prezime;
+    char pol = 'N';
+    int godinaRodjenja;
+
+    // Opisuje da li je osoba u braku (true) ili nije (false)
+    boolean uBraku = true;
+
+    void ispisi(){
+        System.out.println("Ime: " + ime);
+        System.out.println("Prezime: " + prezime);
+        System.out.println("Pol: " + pol);
+        System.out.println("Godina rodjenja: " + godinaRodjenja + ".");
+        System.out.println("U braku: " + uBraku);
+    }
+
+    int izracunajStarost(int trenutnaGodina){
+        int starost = trenutnaGodina - godinaRodjenja;
+        return starost;
+    }
+
+}
---
+++ src/dvocas1_ponedeljak/zadatak1/TestOsoba.java
@@ -0,0 +1,27 @@
+package dvocas1_ponedeljak.zadatak1;
+
+public class TestOsoba {
+
+    public static void main(String[] args){
+       Osoba osoba1 = new Osoba();//null pocetna vrednost
+       Osoba osoba2 = new Osoba();
+
+       osoba1.ime = "Lara";
+       osoba1.prezime = "Peric";
+       osoba1.pol = 'Z';
+       osoba1.godinaRodjenja = 1990;
+       osoba1.uBraku = false;
+
+       osoba2.ime = "Mika";
+       osoba2.prezime = "Mikic";
+       osoba2.pol = 'M';
+       osoba2.godinaRodjenja = 1983;
+        //osoba2.uBraku = true;
+
+       osoba1.ispisi();
+
+       osoba2.ispisi();
+
+       System.out.println(osoba2.izracunajStarost(2025));
+    }
+}
